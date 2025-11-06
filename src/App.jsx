8253338b94798af5d90741ceacb3e2abef0e9237
NikolajWeb <mortensen.nikolaj@gmail.com>
2025-11-06 13:07:40 +0100

    Review components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
-import Pageheader from './components/pageHeader/PageHeader'
+import Pageheader from './components/pageHeader/PageHeader.jsx'
+import Review from './components/Review/review.jsx'
 
 
 function App() {
@@ -7,7 +8,15 @@ function App() {
 
   return (
     <>
-      <Pageheader></Pageheader>
+      <Pageheader />
+
+      <div className="container">
+        <Review title={"Romantisk Gateway"} name={"Lise, 34 år"} description={"Min kæreste og jeg fejrede vores årsdag med et ophold ved Gittes Glamping. Det vil vi helt sikkert gøre igen. personalet var virkelig søde og servicemindede, og maden og stemningen var i top."} />
+        <Review title={"Weekendtur"} name={"Johanne, 22 år"} description={"Jeg blev inviteret med af min veninde. Det var første gang jeg prøvede glamping. Jeg var lidt skeptisk, da jeg ikke bryder mig om at sove udenfor. Men jeg blev positivt overraket. Sengene var gode, og det var slet ikke ubehageligt at vågne op i teltet, som det ellers plejer at være i et normalt telt."} />
+        <Review title={"Familiepakken"} name={"Benjamin, 42 år"} description={"Top karakter til Gittes Glamping herfra! Perfekt blanding af primitivt og luksuriøst. Og ungerne elskede det!"} />
+        <Review title={"Weekendtur"} name={"Peter, 61 år"} description={"Jeg havde en rigtig hyggelig weekend, og maden er i særdeleshed en oplevelse værd. Min hustru synes kanoturen var rigtig idyllisk. Jeg er dog ikke så vild med at sejle."} />
+      </div>
+
     </>
   )
 }
