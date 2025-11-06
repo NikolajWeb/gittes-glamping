e4318d8768565c2e3e02a95c927ddb85f9331e50
NikolajWeb <mortensen.nikolaj@gmail.com>
2025-11-06 09:50:37 +0100

    gg

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Pageheader from './components/pageHeader/PageHeader'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <Pageheader></Pageheader>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/pageHeader/PageHeader.jsx
@@ -0,0 +1,11 @@
+import styles from './pageHeader.module.css'
+
+const Pageheader = () => {
+    return (
+        <header className={styles.header}>
+            <h1>Titel</h1>
+        </header>
+    );
+};
+
+export default Pageheader;
