319554f313ad693fd10f4520e33042a4c5c1921a
NikolajWeb <mortensen.nikolaj@gmail.com>
2025-11-06 11:51:43 +0100

    Pageheader styling og img

--- src/components/pageHeader/PageHeader.jsx
+++ src/components/pageHeader/PageHeader.jsx
@@ -1,9 +1,11 @@
 import styles from './pageHeader.module.css'
+import logo from '/public/logo.png'
 
 const Pageheader = () => {
     return (
         <header className={styles.header}>
-            <h1>Titel</h1>
+            <img src={logo} alt="Gittes Glamping Logo" className={styles.logo} />
+            <h1>Gittes <br /> Glamping</h1>
         </header>
     );
 };
